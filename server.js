0a12085086c50b50446b227426d71fb545d1d2b6
ChrisH <chrishelcoop@me.com>
2026-01-01 16:11:21 +0000

    Added comments

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // server.js
-const express = require('express');
-const app = express();
+const express = require('express'); // Import Express framework
+const app = express(); // Create an Express application
 
-app.use(express.static('public'));
+app.use(express.static('public')); // Serve static files from the 'public' directory
 
-app.listen(3000, () => {
-  console.log('PWA running at http://localhost:3000');
+app.listen(3000, () => { // Start the server on port 3000
+  console.log('PWA running at http://localhost:3000'); // Log a message when the server starts
 });
